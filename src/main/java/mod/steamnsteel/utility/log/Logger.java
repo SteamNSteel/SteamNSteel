ef8a7024a69aafdb0a1c4e920e70fe22f1e07848
Scott Killen <scott@scottkillen.com>
2014-08-15 01:28:34 -0400

    Restructured utility package

--- src/main/java/mod/steamnsteel/utility/log/Logger.java
+++ src/main/java/mod/steamnsteel/utility/log/Logger.java
@@ -14,7 +14,7 @@
  * this program; if not, see <http://www.gnu.org/licenses>.
  */
 
-package mod.steamnsteel.utility;
+package mod.steamnsteel.utility.log;
 
 import mod.steamnsteel.library.Reference;
 import org.apache.logging.log4j.Level;
