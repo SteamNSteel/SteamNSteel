e252d5e06d81ca9dfe82ebcb0baf0f94f782ca9e
Steven Blom <atomicblom@binaryvibrance.net>
2014-11-01 17:46:40 +1030

    Add full set of Block Parts for pipes.

--- src/main/java/mod/steamnsteel/library/ModBlockParts.java
+++ src/main/java/mod/steamnsteel/library/ModBlockParts.java
@@ -8,14 +8,39 @@ import net.minecraft.util.AxisAlignedBB;
  * Created by steblo on 31/10/2014.
  */
 public final class ModBlockParts {
+    private static PartSet pipePartSet;
+
     public static void init() {
         final float scale = 1f / 16f;
         final float e = 0.00001f;
 
-
-        PartSet pipePartSet = BlockPartConfiguration.registerPartSet("pipes");
+        pipePartSet = BlockPartConfiguration.registerPartSet("pipes");
         pipePartSet.createBlockPart("North Connection")
-                .setBoundingBox(0f * scale - e, 0f * scale - e, 0f * scale - e,
-                        8f * scale , 8f * scale, 8f * scale );
+                .setBoundingBox(2f * scale - e, 2f * scale - e, 0f * scale - e,
+                        14f * scale + e , 14f * scale, 3f * scale + e );
+
+        pipePartSet.createBlockPart("South Connection")
+                .setBoundingBox(2f * scale - e, 2f * scale - e, 14f * scale - e,
+                        14f * scale + e , 14f * scale, 16f * scale + e );
+
+        pipePartSet.createBlockPart("East Connection")
+                .setBoundingBox(0f * scale - e, 2f * scale - e, 2f * scale - e,
+                        3f * scale + e, 14f * scale + e, 14f * scale + e);
+
+        pipePartSet.createBlockPart("West Connection")
+                .setBoundingBox(14f * scale - e, 2f * scale - e, 2f * scale - e,
+                        16f * scale + e, 14f * scale + e, 14f * scale + e);
+
+        pipePartSet.createBlockPart("Top Connection")
+                .setBoundingBox(2f * scale - e, 0f * scale - e, 2f * scale - e,
+                        14f * scale + e, 3f * scale + e, 14f * scale + e);
+
+        pipePartSet.createBlockPart("Bottom Connection")
+                .setBoundingBox(2f * scale - e, 14f * scale - e, 2f * scale - e,
+                        14f * scale + e, 16f * scale + e, 14f * scale + e);
+
+        pipePartSet.createBlockPart("Pipe Body")
+                .setBoundingBox(3f * scale - e, 3f * scale - e, 3f * scale - e,
+                        13f * scale + e, 13f * scale + e, 13f * scale + e);
     }
 }
