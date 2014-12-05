4390db250a8c29e6be4121c0c11cd0c33f502472
Steven Blom <atomicblom@binaryvibrance.net>
2014-12-05 19:48:52 +1030

    SchematicWorld need not extend World. Remove all the associated crap. Remove rotation and flipping stuff for now. I'll look at that later.

--- src/main/java/mod/steamnsteel/world/SchematicLoader.java
+++ src/main/java/mod/steamnsteel/world/SchematicLoader.java
@@ -1,39 +1,14 @@
 package mod.steamnsteel.world;
 
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.FMLControlledNamespacedRegistry;
 import cpw.mods.fml.common.registry.GameData;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.block.*;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.block.Block;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntityChest;
-import net.minecraft.tileentity.TileEntitySkull;
-import net.minecraft.util.IProgressUpdate;
-import net.minecraft.world.*;
-import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.EmptyChunk;
-import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.chunk.storage.IChunkLoader;
-import net.minecraft.world.storage.IPlayerFileData;
-import net.minecraft.world.storage.ISaveHandler;
-import net.minecraft.world.storage.WorldInfo;
 import net.minecraftforge.common.util.Constants;
-import net.minecraftforge.common.util.ForgeDirection;
 import org.apache.logging.log4j.Logger;
-import javax.vecmath.Vector3f;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -41,7 +16,7 @@ import java.util.*;
 
 public class SchematicLoader
 {
-    public boolean loadSchematic(EntityPlayer player, File directory, String filename)
+    public boolean loadSchematic(File directory, String filename)
     {
         SchematicWorld schematic = readFromFile(directory, filename);
         if (schematic == null)
@@ -56,16 +31,6 @@ public class SchematicLoader
     private static Logger _logger;
 
     private static final FMLControlledNamespacedRegistry<Block> BLOCK_REGISTRY = GameData.getBlockRegistry();
-    private static final WorldSettings WORLD_SETTINGS = new WorldSettings(0, WorldSettings.GameType.CREATIVE, false, false, WorldType.FLAT);
-    private static final Comparator<ItemStack> BLOCK_COMPARATOR = new Comparator<ItemStack>()
-    {
-        @Override
-        public int compare(ItemStack itemStackA, ItemStack itemStackB)
-        {
-            return itemStackA.getUnlocalizedName().compareTo(itemStackB.getUnlocalizedName());
-        }
-    };
-
 
     public SchematicWorld readFromFile(File file)
     {
@@ -129,7 +94,7 @@ public class SchematicLoader
         short[][][] blocks = new short[width][height][length];
         byte[][][] metadata = new byte[width][height][length];
 
-        Short id = null;
+        Short id;
         Map<Short, Short> oldToNew = new HashMap<Short, Short>();
         if (tagCompound.hasKey(Names.NBT.MAPPING_SCHEMATICA))
         {
@@ -179,59 +144,7 @@ public class SchematicLoader
         return new SchematicWorld(blocks, metadata, tileEntities, width, height, length);
     }
 
-
-    private static ISaveHandler NULL_SAVE_HANDLER = new ISaveHandler()
-    {
-        @Override
-        public WorldInfo loadWorldInfo()
-        {
-            return null;
-        }
-
-        @Override
-        public void checkSessionLock() throws MinecraftException {}
-
-        @Override
-        public IChunkLoader getChunkLoader(WorldProvider provider)
-        {
-            return null;
-        }
-
-        @Override
-        public void saveWorldInfoWithPlayer(WorldInfo info, NBTTagCompound compound) {}
-
-        @Override
-        public void saveWorldInfo(WorldInfo info) {}
-
-        @Override
-        public IPlayerFileData getSaveHandler()
-        {
-            return null;
-        }
-
-        @Override
-        public void flush() {}
-
-        @Override
-        public File getWorldDirectory()
-        {
-            return null;
-        }
-
-        @Override
-        public File getMapFileFromName(String name)
-        {
-            return null;
-        }
-
-        @Override
-        public String getWorldDirectoryName()
-        {
-            return null;
-        }
-    };
-
-    public static class SchematicWorld extends World
+    public static class SchematicWorld
     {
         private short[][][] blocks;
         private byte[][][] metadata;
@@ -240,11 +153,8 @@ public class SchematicLoader
         private short height;
         private short length;
 
-//        public final Vector3i position = new Vector3i();
-
         public SchematicWorld()
         {
-            super(NULL_SAVE_HANDLER, "Schematica", WORLD_SETTINGS, null, null);
             this.blocks = null;
             this.metadata = null;
             this.tileEntities.clear();
@@ -269,7 +179,6 @@ public class SchematicLoader
                 this.tileEntities.addAll(tileEntities);
                 for (TileEntity tileEntity : this.tileEntities)
                 {
-                    tileEntity.setWorldObj(this);
                     tileEntity.getBlockType();
                     try
                     {
@@ -280,11 +189,6 @@ public class SchematicLoader
                     }
                 }
             }
-        }
-
-        public SchematicWorld(short width, short height, short length)
-        {
-            this(null, null, null, width, height, length);
         }
 
         public int getBlockIdRaw(int x, int y, int z)
@@ -306,37 +210,11 @@ public class SchematicLoader
             return BLOCK_REGISTRY.getObjectById(getBlockIdRaw(x, y, z));
         }
 
-        @Override
         public Block getBlock(int x, int y, int z)
         {
             return BLOCK_REGISTRY.getObjectById(getBlockId(x, y, z));
         }
 
-        public boolean setBlock(int x, int y, int z, Block block, int metadata)
-        {
-            return setBlock(x, y, z, block, metadata, 0);
-        }
-
-        @Override
-        public boolean setBlock(int x, int y, int z, Block block, int metadata, int flags)
-        {
-            if (x < 0 || y < 0 || z < 0 || x >= this.width || y >= this.height || z >= this.length)
-            {
-                return false;
-            }
-
-            final int id = BLOCK_REGISTRY.getId(block);
-            if (id == -1)
-            {
-                return false;
-            }
-
-            this.blocks[x][y][z] = (short) id;
-            this.metadata[x][y][z] = (byte) metadata;
-            return true;
-        }
-
-        @Override
         public TileEntity getTileEntity(int x, int y, int z)
         {
             for (TileEntity tileEntity : this.tileEntities)
@@ -349,47 +227,6 @@ public class SchematicLoader
             return null;
         }
 
-        @Override
-        public void setTileEntity(int x, int y, int z, TileEntity tileEntity)
-        {
-            if (x < 0 || y < 0 || z < 0 || x >= this.width || y >= this.height || z >= this.length)
-            {
-                return;
-            }
-
-            removeTileEntity(x, y, z);
-
-            this.tileEntities.add(tileEntity);
-        }
-
-        @Override
-        public void removeTileEntity(int x, int y, int z)
-        {
-            final Iterator<TileEntity> iterator = this.tileEntities.iterator();
-            while (iterator.hasNext())
-            {
-                final TileEntity tileEntity = iterator.next();
-                if (tileEntity.xCoord == x && tileEntity.yCoord == y && tileEntity.zCoord == z)
-                {
-                    iterator.remove();
-                }
-            }
-        }
-
-        @SideOnly(Side.CLIENT)
-        @Override
-        public int getSkyBlockTypeBrightness(EnumSkyBlock skyBlock, int x, int y, int z)
-        {
-            return 15;
-        }
-
-        @Override
-        public float getLightBrightness(int x, int y, int z)
-        {
-            return 1.0f;
-        }
-
-        @Override
         public int getBlockMetadata(int x, int y, int z)
         {
             if (x < 0 || y < 0 || z < 0 || x >= this.width || y >= this.height || z >= this.length)
@@ -399,28 +236,6 @@ public class SchematicLoader
             return this.metadata[x][y][z];
         }
 
-        @Override
-        public boolean isBlockNormalCubeDefault(int x, int y, int z, boolean _default)
-        {
-            Block block = getBlock(x, y, z);
-            if (block == null)
-            {
-                return false;
-            }
-            if (block.isNormalCube())
-            {
-                return true;
-            }
-            return _default;
-        }
-
-        @Override
-        protected int func_152379_p()
-        {
-            return 0;
-        }
-
-        @Override
         public boolean isAirBlock(int x, int y, int z)
         {
             Block block = getBlock(x, y, z);
@@ -428,13 +243,7 @@ public class SchematicLoader
             {
                 return true;
             }
-            return block.isAir(this, x, y, z);
-        }
-
-        @Override
-        public BiomeGenBase getBiomeGenForCoords(int x, int z)
-        {
-            return BiomeGenBase.jungle;
+            return block.isAir(null, x, y, z);
         }
 
         public int getWidth()
@@ -447,274 +256,15 @@ public class SchematicLoader
             return this.length;
         }
 
-        @Override
         public int getHeight()
         {
             return this.height;
-        }
-
-        @SideOnly(Side.CLIENT)
-        @Override
-        public boolean extendedLevelsInChunkCache()
-        {
-            return false;
-        }
-
-        @Override
-        protected IChunkProvider createChunkProvider()
-        {
-            return new ChunkProviderSchematic(this);
-        }
-
-        @Override
-        public Entity getEntityByID(int id)
-        {
-            return null;
-        }
-
-        @Override
-        public boolean blockExists(int x, int y, int z)
-        {
-            return false;
-        }
-
-        @Override
-        public boolean setBlockMetadataWithNotify(int x, int y, int z, int metadata, int flag)
-        {
-            this.metadata[x][y][z] = (byte) (metadata & 0xFF);
-            return true;
-        }
-
-        @Override
-        public boolean isSideSolid(int x, int y, int z, ForgeDirection side)
-        {
-            return isSideSolid(x, y, z, side, false);
-        }
-
-        @Override
-        public boolean isSideSolid(int x, int y, int z, ForgeDirection side, boolean _default)
-        {
-            Block block = getBlock(x, y, z);
-            if (block == null)
-            {
-                return false;
-            }
-            return block.isSideSolid(this, x, y, z, side);
-        }
-
-        public void setTileEntities(List<TileEntity> tileEntities)
-        {
-            this.tileEntities.clear();
-            this.tileEntities.addAll(tileEntities);
-            for (TileEntity tileEntity : this.tileEntities)
-            {
-                tileEntity.setWorldObj(this);
-                try
-                {
-                    tileEntity.validate();
-                } catch (Exception e)
-                {
-                    _logger.error(String.format("TileEntity validation for %s failed!", tileEntity.getClass()), e);
-                }
-            }
         }
 
         public List<TileEntity> getTileEntities()
         {
             return this.tileEntities;
         }
-
-        public void refreshChests()
-        {
-            for (TileEntity tileEntity : this.tileEntities)
-            {
-                if (tileEntity instanceof TileEntityChest)
-                {
-                    TileEntityChest tileEntityChest = (TileEntityChest) tileEntity;
-                    tileEntityChest.adjacentChestChecked = false;
-                    tileEntityChest.checkForAdjacentChests();
-                }
-            }
-        }
-
-        public void flip()
-        {
-        /*
-        int tmp;
-		for (int x = 0; x < this.width; x++) {
-			for (int y = 0; y < this.height; y++) {
-				for (int z = 0; z < (this.length + 1) / 2; z++) {
-					tmp = this.blocks[x][y][z];
-					this.blocks[x][y][z] = this.blocks[x][y][this.length - 1 - z];
-					this.blocks[x][y][this.length - 1 - z] = tmp;
-
-					if (z == this.length - 1 - z) {
-						this.metadata[x][y][z] = BlockInfo.getTransformedMetadataFlip(this.blocks[x][y][z], this.metadata[x][y][z]);
-					} else {
-						tmp = this.metadata[x][y][z];
-						this.metadata[x][y][z] = BlockInfo.getTransformedMetadataFlip(this.blocks[x][y][z], this.metadata[x][y][this.length - 1 - z]);
-						this.metadata[x][y][this.length - 1 - z] = BlockInfo.getTransformedMetadataFlip(this.blocks[x][y][this.length - 1 - z], tmp);
-					}
-				}
-			}
-		}
-
-		TileEntity tileEntity;
-		for (int i = 0; i < this.tileEntities.size(); i++) {
-			tileEntity = this.tileEntities.get(i);
-			tileEntity.zCoord = this.length - 1 - tileEntity.zCoord;
-			tileEntity.blockMetadata = this.metadata[tileEntity.xCoord][tileEntity.yCoord][tileEntity.zCoord];
-
-			if (tileEntity instanceof TileEntitySkull && tileEntity.blockMetadata == 0x1) {
-				TileEntitySkull skullTileEntity = (TileEntitySkull) tileEntity;
-				int angle = skullTileEntity.func_82119_b();
-				int base = 0;
-				if (angle <= 7) {
-					base = 4;
-				} else {
-					base = 12;
-				}
-
-				skullTileEntity.setSkullRotation((2 * base - angle) & 15);
-			}
-		}
-
-		refreshChests();
-		*/
-        }
-
-        public void rotate()
-        {
-            short[][][] localBlocks = new short[this.length][this.height][this.width];
-            byte[][][] localMetadata = new byte[this.length][this.height][this.width];
-
-            for (int y = 0; y < this.height; y++)
-            {
-                for (int z = 0; z < this.length; z++)
-                {
-                    for (int x = 0; x < this.width; x++)
-                    {
-                        try
-                        {
-                            getBlock(x, y, this.length - 1 - z).rotateBlock(this, x, y, this.length - 1 - z, ForgeDirection.UP);
-                        } catch (Exception e)
-                        {
-                            _logger.debug("Failed to rotate block!", e);
-                        }
-                        localBlocks[z][y][x] = this.blocks[x][y][this.length - 1 - z];
-                        localMetadata[z][y][x] = this.metadata[x][y][this.length - 1 - z];
-                    }
-                }
-            }
-
-            this.blocks = localBlocks;
-            this.metadata = localMetadata;
-
-            int coord;
-            for (TileEntity tileEntity : this.tileEntities)
-            {
-                coord = tileEntity.zCoord;
-                tileEntity.zCoord = tileEntity.xCoord;
-                tileEntity.xCoord = this.length - 1 - coord;
-                tileEntity.blockMetadata = this.metadata[tileEntity.xCoord][tileEntity.yCoord][tileEntity.zCoord];
-
-                if (tileEntity instanceof TileEntitySkull && tileEntity.blockMetadata == 0x1)
-                {
-                    TileEntitySkull skullTileEntity = (TileEntitySkull) tileEntity;
-                    skullTileEntity.func_145903_a((skullTileEntity.func_145906_b() + 12) & 15);
-                }
-            }
-
-            short tmp = this.width;
-            this.width = this.length;
-            this.length = tmp;
-
-            refreshChests();
-        }
-
-        public Vector3f dimensions()
-        {
-            return new Vector3f(this.width, this.height, this.length);
-        }
-    }
-
-    static class ChunkProviderSchematic implements IChunkProvider
-    {
-        private Chunk emptyChunk;
-
-        public ChunkProviderSchematic(World world)
-        {
-            this.emptyChunk = new EmptyChunk(world, 0, 0);
-        }
-
-        @Override
-        public boolean chunkExists(int x, int y)
-        {
-            return true;
-        }
-
-        @Override
-        public Chunk provideChunk(int x, int y)
-        {
-            return this.emptyChunk;
-        }
-
-        @Override
-        public Chunk loadChunk(int x, int y)
-        {
-            return this.emptyChunk;
-        }
-
-        @Override
-        public void populate(IChunkProvider provider, int x, int y) {}
-
-        @Override
-        public boolean saveChunks(boolean saveExtra, IProgressUpdate progressUpdate)
-        {
-            return true;
-        }
-
-        @Override
-        public boolean unloadQueuedChunks()
-        {
-            return false;
-        }
-
-        @Override
-        public boolean canSave()
-        {
-            return false;
-        }
-
-        @Override
-        public String makeString()
-        {
-            return "SchematicChunkCache";
-        }
-
-        @Override
-        public List getPossibleCreatures(EnumCreatureType creatureType, int x, int y, int z)
-        {
-            return null;
-        }
-
-        @Override
-        public ChunkPosition func_147416_a(World world, String name, int x, int y, int z)
-        {
-            return null;
-        }
-
-        @Override
-        public int getLoadedChunkCount()
-        {
-            return 0;
-        }
-
-        @Override
-        public void recreateStructures(int x, int y) {}
-
-        @Override
-        public void saveExtraData() {}
     }
 
     public static final class Names
@@ -732,5 +282,4 @@ public class SchematicLoader
             public static final String TILE_ENTITIES = "TileEntities";
         }
     }
-
 }
