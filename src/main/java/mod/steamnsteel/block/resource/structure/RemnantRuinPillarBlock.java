77387d0b705d12ab44a19189eaff35a9b84aeef4
Steven Blom <atomicblom@binaryvibrance.net>
2016-06-09 23:08:00 +0930

    Add cap state to pillars

--- src/main/java/mod/steamnsteel/block/resource/structure/RemnantRuinPillarBlock.java
+++ src/main/java/mod/steamnsteel/block/resource/structure/RemnantRuinPillarBlock.java
@@ -18,16 +18,53 @@ package mod.steamnsteel.block.resource.structure;
 
 import mod.steamnsteel.block.*;
 import mod.steamnsteel.tileentity.RemnantRuinPillarTE;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class RemnantRuinPillarBlock extends SteamNSteelDirectionalBlock
 {
+    public static PropertyEnum<PillarCaps> pillarCapsProperty = PropertyEnum.create("caps", PillarCaps.class);
+
     public RemnantRuinPillarBlock()
     {
         super(Material.ROCK);
+        setDefaultState(blockState.getBaseState().withProperty(pillarCapsProperty, PillarCaps.BOTH));
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, BlockHorizontal.FACING, pillarCapsProperty);
+    }
+
+
+
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos)
+    {
+        boolean hasPillarAbove = worldIn.getBlockState(pos.offset(EnumFacing.UP)).getBlock() == this;
+        boolean hasPillarBelow = worldIn.getBlockState(pos.offset(EnumFacing.DOWN)).getBlock() == this;
+
+        PillarCaps caps = PillarCaps.NONE;
+
+        if (!hasPillarAbove && !hasPillarBelow) {
+            caps = PillarCaps.BOTH;
+        } else if (hasPillarAbove && !hasPillarBelow) {
+            caps = PillarCaps.BOTTOM;
+        } else if (!hasPillarAbove && hasPillarBelow) {
+            caps = PillarCaps.TOP;
+        }
+
+        return super.getActualState(state, worldIn, pos).withProperty(pillarCapsProperty, caps);
     }
 
     @Override
@@ -47,5 +84,18 @@ public class RemnantRuinPillarBlock extends SteamNSteelDirectionalBlock
     public boolean hasTileEntity(IBlockState blockState)
     {
         return true;
+    }
+
+    public enum PillarCaps implements IStringSerializable {
+        TOP,
+        BOTTOM,
+        BOTH,
+        NONE;
+
+        @Override
+        public String getName()
+        {
+            return this.name().toLowerCase();
+        }
     }
 }
